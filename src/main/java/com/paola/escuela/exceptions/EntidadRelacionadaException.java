5ed67264835723972557a6bdd74a05e8b1d4efd2
Paola Diaz <paolaestefaniadiaz3@gmail.com>
2026-08-11 13:00:26 -0600

    Agregue EntidadRelacionadaException

---
+++ src/main/java/com/paola/escuela/exceptions/EntidadRelacionadaException.java
@@ -0,0 +1,7 @@
+package com.paola.escuela.exceptions;
+
+public class EntidadRelacionadaException extends RuntimeException {
+  public EntidadRelacionadaException(String message) {
+    super(message);
+  }
+}
